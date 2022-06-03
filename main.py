ade270ac070b0e885fe82d227c12618d94f0ad3a
mark <markpackham1@gmail.com>
2022-06-03 10:05:11 +0100

    console.log specific element
    https://youtu.be/vxqBm6_0vyk?t=1032

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import random
 
+output_el = Element('output').element
+console.log(output_el)
+
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 pyscript.write('output', f"{arr}")
