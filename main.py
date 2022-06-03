18b87b54281a64bbc25296278dc483e9387e2d01
mark <markpackham1@gmail.com>
2022-06-03 09:54:22 +0100

    demo array in shuffled
    https://youtu.be/vxqBm6_0vyk?t=947

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import numpy as np
+import random
 
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 pyscript.write('output', f"{arr}")
 
 def shuffle_array(*args):
-    pyscript.write('output','This works')
+
+    shuffled = sorted(arr, key=lambda k: random.random())
+    pyscript.write('output',shuffled)
