93cc178ff7db96d3738f205fecad77505fce8a53
mark <markpackham1@gmail.com>
2022-06-03 09:45:51 +0100

    demo pys-onClick
    https://youtu.be/vxqBm6_0vyk?t=888

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5])
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-pyscript.write('output', f"My array is {arr}")
+pyscript.write('output', f"{arr}")
+
+def shuffle_array(*args):
+    pyscript.write('output','This works')
